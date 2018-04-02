620420135bf8eef7ce5a9701707b1d116b495a22
omigibson <oumie.gibba@gmail.com>
2018-04-02 19:31:40 +0200

    Added interactive form for adding new music

--- js/main.js
+++ js/main.js
@@ -24,39 +24,86 @@ function searchResult() {
 
 //Code for adding new content below
 
-document.getElementById("addButton").addEventListener("click", addTrack);
+document.getElementById('addCategory').addEventListener('change', function (){
+	formInputFields();
+	inputFields.title.style.display = 'none';
+	inputFields.name.style.display = 'none';
+	inputFields.artist.style.display = 'none';
+	inputFields.album.style.display = 'none';
+	inputFields.genre.style.display = 'none';
+})
+
+let addCategory = document.getElementById('addCategory');
+addCategory.addEventListener('click', showForm);
+
+function showForm() {
+	formInputFields()
+	switch (addCategory.value) {
+		case 'Track':
+			inputFields.title.style.display = 'inline-block';
+			inputFields.artist.style.display = 'inline-block';
+			inputFields.album.style.display = 'inline-block';
+			inputFields.genre.style.display = 'inline-block';
+			break;
+		case 'Artist':
+			inputFields.name.style.display = 'inline-block';
+			inputFields.genre.style.display = 'inline-block';
+			break;
+		case 'Album':
+			inputFields.title.style.display = 'inline-block';
+			inputFields.artist.style.display = 'inline-block';
+			inputFields.genre.style.display = 'inline-block';
+			break;
+		case 'Playlist':
+			inputFields.title.style.display = 'inline-block';
+			break;
+	}
+
+}
+
+function formInputFields() {
+	return inputFields = {
+		title: document.getElementById('title'),
+		name: document.getElementById('name'),
+		artist: document.getElementById('artist'),
+		album: document.getElementById('album'),
+		genre: document.getElementById('genre')
+	}
+}
+
+document.getElementById('addButton').addEventListener('click', addTrack);
 
 function addTrack() {
-	let title = document.getElementById('title');
-	let artist = document.getElementById('artist');
-	let album = document.getElementById('album');
-	let genre = document.getElementById('genre');
-
-	var track = {
+	/*var track = {
 		title: title.value,
 		artists: artist.value,
 		album: album.value,
 		genres: genre.value
+	}*/
+
+	var artistInfo = {
+		name: name.value,
+		genres: genre.value
 	}
-	
-	console.log(track);
-	
+
+	console.log(artistInfo);
+
 	var postOptions = {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(track)
-		};
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(artistInfo)
+	};
 	console.log(postOptions);
 
-	fetch('https://folksa.ga/api/tracks?key=flat_eric', postOptions)
-	.then((response) => response.json())
-  .then((postedTrack) => {
-    console.log(postedTrack);
-  })
-	.catch(function (error) {  
-    console.log('Request failed', error);  
-  });
+	fetch('https://folksa.ga/api/artists?key=flat_eric', postOptions)
+		.then((response) => response.json())
+		.then((postedTrack) => {
+			console.log(postedTrack);
+		})
+		.catch((error) => {
+			console.log('Request failed: ', error);
+		});
 }
